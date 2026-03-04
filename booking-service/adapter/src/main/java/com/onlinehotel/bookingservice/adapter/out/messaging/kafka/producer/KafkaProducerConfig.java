55b951d66c70493caa37d792acd0dfd3d6a3187c
БульбочкаВ <disablemon@icloud.com>
2026-03-04 12:59:11 +0300

    Kafka producer: BookingCreatedEvent

---
+++ booking-service/adapter/src/main/java/com/onlinehotel/bookingservice/adapter/out/messaging/kafka/producer/KafkaProducerConfig.java
@@ -0,0 +1,48 @@
+package com.onlinehotel.bookingservice.adapter.out.messaging.kafka.producer;
+
+import com.onlinehotel.bookingservice.application.dto.event.BookingCreatedEvent;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.converter.StringJacksonJsonMessageConverter;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+@FieldDefaults(level= AccessLevel.PRIVATE)
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServer;
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class.getName());
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<Long, BookingCreatedEvent> producerBookingFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<Long, BookingCreatedEvent> kafkaTemplate() {
+        KafkaTemplate<Long, BookingCreatedEvent> template = new KafkaTemplate<>(producerBookingFactory());
+        template.setMessageConverter(new StringJacksonJsonMessageConverter());
+        return template;
+    }
+}
